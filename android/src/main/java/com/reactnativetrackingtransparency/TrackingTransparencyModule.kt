374ea8b05a571653ed4cdde3bd52fbd45049e1ea
Marc Rousavy <marcrousavy@hotmail.com>
2020-12-10 18:01:25 +0100

    Update TrackingTransparencyModule.kt

--- android/src/main/java/com/reactnativetrackingtransparency/TrackingTransparencyModule.kt
+++ android/src/main/java/com/reactnativetrackingtransparency/TrackingTransparencyModule.kt
@@ -6,19 +6,7 @@ import com.facebook.react.bridge.ReactMethod
 import com.facebook.react.bridge.Promise
 
 class TrackingTransparencyModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
-
     override fun getName(): String {
         return "TrackingTransparency"
     }
-
-    // Example method
-    // See https://facebook.github.io/react-native/docs/native-modules-android
-    @ReactMethod
-    fun multiply(a: Int, b: Int, promise: Promise) {
-    
-      promise.resolve(a * b)
-    
-    }
-
-    
 }
