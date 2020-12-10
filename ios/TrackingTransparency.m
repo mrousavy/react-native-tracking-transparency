383f5cd54e61715d944e022b88da373ff7d30a5c
Marc Rousavy <marcrousavy@hotmail.com>
2020-12-10 18:01:37 +0100

    Implement iOS native functions

--- ios/TrackingTransparency.m
+++ ios/TrackingTransparency.m
@@ -1,19 +1,41 @@
 #import "TrackingTransparency.h"
+#import <AppTrackingTransparency/AppTrackingTransparency.h>
 
 @implementation TrackingTransparency
 
 RCT_EXPORT_MODULE()
 
-// Example method
-// See // https://facebook.github.io/react-native/docs/native-modules-ios
-RCT_REMAP_METHOD(multiply,
-                 multiplyWithA:(nonnull NSNumber*)a withB:(nonnull NSNumber*)b
-                 withResolver:(RCTPromiseResolveBlock)resolve
-                 withRejecter:(RCTPromiseRejectBlock)reject)
+RCT_EXPORT_METHOD(getTrackingStatus:(RCTPromiseResolveBlock)resolve rejector:(RCTPromiseRejectBlock)reject)
 {
-  NSNumber *result = @([a floatValue] * [b floatValue]);
+    if (@available(iOS 14, *)) {
+        resolve([TrackingTransparency convertTrackingStatusToString:[ATTrackingManager trackingAuthorizationStatus]]);
+    } else {
+        resolve(@"unavailable");
+    }
+}
 
-  resolve(result);
+RCT_EXPORT_METHOD(requestTrackingPermission:(RCTPromiseResolveBlock)resolve rejector:(RCTPromiseRejectBlock)reject)
+{
+    if (@available(iOS 14, *)) {
+        [ATTrackingManager requestTrackingAuthorizationWithCompletionHandler:^(ATTrackingManagerAuthorizationStatus status) {
+            resolve([TrackingTransparency convertTrackingStatusToString:status]);
+        }];
+    } else {
+        resolve(@"unavailable");
+    }
+}
+
++ (NSString *) convertTrackingStatusToString:(ATTrackingManagerAuthorizationStatus) status API_AVAILABLE(ios(14)) {
+    switch (status) {
+        case ATTrackingManagerAuthorizationStatusDenied:
+            return @"denied";
+        case ATTrackingManagerAuthorizationStatusAuthorized:
+            return @"authorized";
+        case ATTrackingManagerAuthorizationStatusRestricted:
+            return @"restricted";
+        case ATTrackingManagerAuthorizationStatusNotDetermined:
+            return @"not-determined";
+    }
 }
 
 @end
