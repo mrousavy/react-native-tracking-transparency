66352e8a6a95dd4c0068286dab811481c9b94686
quynhMI <quynhMI@gmail.com>
2022-06-08 11:02:07 +0700

    fix error using view on background thread

--- ios/TrackingTransparency.m
+++ ios/TrackingTransparency.m
@@ -20,27 +20,29 @@ RCT_EXPORT_METHOD(getTrackingStatus:(RCTPromiseResolveBlock)resolve rejector:(RC
 
 RCT_EXPORT_METHOD(requestTrackingPermission:(RCTPromiseResolveBlock)resolve rejector:(RCTPromiseRejectBlock)reject)
 {
-    if (@available(iOS 14, *)) {
-        if (UIApplication.sharedApplication.applicationState == UIApplicationStateActive) {
-            [ATTrackingManager requestTrackingAuthorizationWithCompletionHandler:^(ATTrackingManagerAuthorizationStatus status) {
-                resolve([TrackingTransparency convertTrackingStatusToString:status]);
-            }];
-        }
-        else {
-            __weak TrackingTransparency *weakSelf = self;
-            
-            self._observer = [[NSNotificationCenter defaultCenter] addObserverForName:UIApplicationDidBecomeActiveNotification object:nil queue: [NSOperationQueue mainQueue] usingBlock:^(NSNotification * _Nonnull note) {
-                
+    dispatch_async(dispatch_get_main_queue(), ^{
+        if (@available(iOS 14, *)) {
+            if (UIApplication.sharedApplication.applicationState == UIApplicationStateActive) {
                 [ATTrackingManager requestTrackingAuthorizationWithCompletionHandler:^(ATTrackingManagerAuthorizationStatus status) {
                     resolve([TrackingTransparency convertTrackingStatusToString:status]);
                 }];
+            }
+            else {
+                __weak TrackingTransparency *weakSelf = self;
                 
-                [[NSNotificationCenter defaultCenter] removeObserver:weakSelf._observer];
-            }];
+                self._observer = [[NSNotificationCenter defaultCenter] addObserverForName:UIApplicationDidBecomeActiveNotification object:nil queue: [NSOperationQueue mainQueue] usingBlock:^(NSNotification * _Nonnull note) {
+                    
+                    [ATTrackingManager requestTrackingAuthorizationWithCompletionHandler:^(ATTrackingManagerAuthorizationStatus status) {
+                        resolve([TrackingTransparency convertTrackingStatusToString:status]);
+                    }];
+                    
+                    [[NSNotificationCenter defaultCenter] removeObserver:weakSelf._observer];
+                }];
+            }
+        } else {
+            resolve(@"unavailable");
         }
-    } else {
-        resolve(@"unavailable");
-    }
+    });
 }
 
 + (NSString *) convertTrackingStatusToString:(ATTrackingManagerAuthorizationStatus) status API_AVAILABLE(ios(14)) {
